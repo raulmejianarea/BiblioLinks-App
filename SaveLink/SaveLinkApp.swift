610d0fa48706fb2e80792a281d2ef0e43c05dca7
Rauldev <raulmejianarea@gmail.com>
2023-08-24 18:58:06 +0200

    Initial Commit

---
+++ SaveLink/SaveLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveLinkApp.swift
+//  SaveLink
+//
+//  Created by raul Mejia on 24/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SaveLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
